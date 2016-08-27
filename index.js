6b4551d7b0e7a6c60178d4da07ab0ed8941a216a
Ben Pang <benjinpang@gmail.com>
2016-08-27 16:33:21 -0700

    cool stuff is happening yo

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 p=require('pokemon-go-node-api')
+E=require('express')
+app=E()
 a=process.argv.slice(2)
 l=console.log
+c=p.GetLocationCoords
+Pk=[]
+stat='walking'
+
 process.stdout.write('\033c')
-p.init(a[0],a[1],a[2]||{type:'name',name:'Galvanize San Francisco'},a[3]||'google',e=>{
+p.init(a[1],a[2],a[3]||{type:'name',name:'Galvanize San Francisco'},a[4]||'google',e=>{
   if(e)throw e;
+  app.use(E.static(__dirname+'/public'))
+  app.get('/loc',(_,y)=>{
+    y.setHeader('Content-Type','application/json')
+    y.json({key:a[0],lat:c().latitude,long:c().longitude,pk:Pk,stat:stat})
+  })
   l('Location:',p.playerInfo.locationName+';','lat',p.playerInfo.latitude+',','long',p.playerInfo.longitude)
   p.GetProfile((e,P)=>{
     if(e)throw e;
     l('Username:',P.username)
+    app.set('title',P.username)
     l('Poke storage:',P.poke_storage)
     l('Item storage:',P.item_storage)
     l('Pokecoins:',P.currency[0].amount)
@@ -15,14 +27,17 @@ p.init(a[0],a[1],a[2]||{type:'name',name:'Galvanize San Francisco'},a[3]||'googl
     setInterval(_=>{
       p.Heartbeat((e,m)=>{
         if(e)throw e;
+        Pk=[]
         m.cells.map(x=>{
-          x.NearbyPokemon[0]&&l(p.pokemonlist[x.NearbyPokemon[0].PokedexNumber-1].name,'is nearby')
+          x.NearbyPokemon[0]&&Pk.push(p.pokemonlist[x.NearbyPokemon[0].PokedexNumber-1].name)
           x.MapPokemon.map(X=>{
             l('Catching',pk=p.pokemonlist[X.PokedexTypeId-1].name)
-            p.encounterPokemon(X,_=>{
+            p.EncounterPokemon(X,_=>{
               l('A wild ',pk,'appeared!')
-              p.catchPokemon(X,1,1.95,1,1,(a,b)=>{
-                l(['ERR','Caught','Escaped','Fled','Missed'][b.Status])
+              stat='catching '+pk
+              p.CatchPokemon(X,1,1.95,1,1,(a,b)=>{
+                l(st['ERR','Caught','Escaped','Fled','Missed'][b.Status])
+                stat=`catching ${pk} - `+st
               })
             })
           })
@@ -30,4 +45,16 @@ p.init(a[0],a[1],a[2]||{type:'name',name:'Galvanize San Francisco'},a[3]||'googl
       })
     },5000)
   })
+  setInterval(_=>{
+    p.SetLocation({
+      type:'coords',
+      coords:{
+        latitude:c().latitude+(Math.random()*2-1)/1e5,
+        longitude:c().longitude+(Math.random()*2-1)/1e5,
+        altitude:c().altitude
+      }
+    },_=>{})
+  },500)
 })
+
+app.listen(8080,'127.0.0.1')
