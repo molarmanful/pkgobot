d581592d4da6c31686f3a308f4a2a5f5b5d4fefc
Ben Pang <benjinpang@gmail.com>
2016-08-28 08:08:39 -0700

    welp

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pkgobot
-A Pokemon GO bot.
+A Pokémon GO bot created at /hack 2016.
+
+##Installation
+Simply download the repo as a `.zip` file (or clone in desktop).
+
+##Usage
+Make sure you have a Pokémon GO account already, preferably one that you are not using (to prevent any risk of your favorite account from getting banned).
+
+`cd` into the directory if you haven't already. `node index.js` takes 3-5 command line arguments, in order:
+
+1. Google Maps API key (both the Geocoder and Javascript APIs). See the _Token_ section.
+2. Email/Username.
+3. Password.
+4. Optional. Defaults to `{type:'name',name:'Galvanize San Francisco'}`. Location as a JSON object; can be one of (as examples):
+    * `{type:'name',name:'Times Square'}`
+    * `{type:'coords',coords:{latitude:0,longitude:0,altitude:0}}`
+5. Optional. Defaults to `google`. Can be either `google` or `ptc` depending on what account you used for arguments 2 and 3.
+
+After successfully running the command, web output is hosted at `http://localhost:8080`
+
+##Token
+1. Go to [The Google Developers Console](https://console.developers.google.com).
+2. Create a project, and in the "Library" tab at the sidebar, add the Google Maps Geocoder and Javascript APIs.
+3. Click the "Credentials" tab at the sidebar and click "Create credentials > API Key > Browser Key."
+4. Finish creating the browser key. You now have your API key!
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-p=require('pokemon-go-node-api')
+p=require('pogobuf')
 E=require('express')
 app=E()
 a=process.argv.slice(2)
@@ -7,11 +7,13 @@ c=p.GetLocationCoords
 Pk=[]
 pkmn=[]
 egg=[]
+item=[]
 stat='walking'
 lvl=''
+stop=[]
 
 process.stdout.write('\033c')
-p.init(a[1],a[2],a[3]||{type:'name',name:'Galvanize San Francisco'},a[4]||'google',e=>{
+p.init(a[1],a[2],a[3]||{type:'name',name:'London'},a[4]||'google',e=>{
   if(e)throw e;
   l('Location:',p.playerInfo.locationName+';','lat',p.playerInfo.latitude+',','long',p.playerInfo.longitude)
   p.GetProfile((e,P)=>{
@@ -24,15 +26,17 @@ p.init(a[1],a[2],a[3]||{type:'name',name:'Galvanize San Francisco'},a[4]||'googl
     app.use(E.static(__dirname+'/public'))
     app.get('/loc',(_,y)=>{
       y.setHeader('Content-Type','application/json')
-      y.json({user:P.username,coins:P.currency[0].amount,dust:P.currency[1].amount,key:a[0],lat:c().latitude,long:c().longitude,pk:Pk,stat:stat,pkmn:pkmn,egg:egg,lvl:lvl})
+      y.json({user:P.username,coins:P.currency[0].amount,dust:P.currency[1].amount,key:a[0],lat:c().latitude,long:c().longitude,pk:Pk,stat:stat,pkmn:pkmn,egg:egg,lvl:lvl,item:item,stop:stop})
     })
     setInterval(_=>{
       p.GetInventory((e,i)=>{
         egg=[]
         pkmn=[]
+        item=[]
         e||i.inventory_delta.inventory_items.map(x=>{
-          x.inventory_item_data.pokemon&&(x.inventory_item_data.pokemon.is_egg?egg.push(x.inventory_item_data.pokemon.egg_km_walked_target+' Egg'):pkmn.push(x.inventory_item_data.pokemon.cp+'CP '+p.pokemonlist[x.inventory_item_data.pokemon.pokemon_id-1].name))
+          x.inventory_item_data.pokemon&&(x.inventory_item_data.pokemon.is_egg?egg.push(x.inventory_item_data.pokemon.egg_km_walked_target+'km\tEgg'):pkmn.push(`CP${x.inventory_item_data.pokemon.cp}\t`+p.pokemonlist[x.inventory_item_data.pokemon.pokemon_id-1].name))
           x.inventory_item_data.player_stats&&(lvl='lvl'+x.inventory_item_data.player_stats.level)
+          x.inventory_item_data.item&&item.push(x.inventory_item_data.item.count+'\t'+x.inventory_item_data.item.item_id)
         })
       })
       p.Heartbeat((e,m)=>{
@@ -45,14 +49,14 @@ p.init(a[1],a[2],a[3]||{type:'name',name:'Galvanize San Francisco'},a[4]||'googl
             p.EncounterPokemon(X,_=>{
               l('A wild',pk,'appeared!')
               p.CatchPokemon(X,1,1.95,1,1,(a,b)=>{
-                l(st=['ERR','Caught','Escaped','Fled','Missed'][b.Status])
-                stat=st=='Caught'||st=='ERR'?'walking':`catching ${pk} - `+st
+                  b&&(st=['ERR','Caught','Escaped','Fled','Missed'][b.Status],stat=st=='Caught'||st=='ERR'?'walking':`catching ${pk} - `+st)
               })
             })
           })
           x.Fort.map(X=>{
+            stop.push({lat:X.Latitude,lng:X.Longitude})
             X.FortType&&X.Enabled&&p.GetFort(X.FortId,X.Latitude,X.Longitude,(a,b)=>{
-              b&&b.result==1&&l(b,'Used PokeStop')
+              b&&b.result==1&&(l(b,'Used PokeStop'))
             })
           })
         })
@@ -63,8 +67,8 @@ p.init(a[1],a[2],a[3]||{type:'name',name:'Galvanize San Francisco'},a[4]||'googl
     p.SetLocation({
       type:'coords',
       coords:{
-        latitude:c().latitude+(Math.random()*2-1)/1e5,
-        longitude:c().longitude+(Math.random()*2-1)/1e5,
+        latitude:c().latitude+(Math.random()*2-1)/5e4,
+        longitude:c().longitude+(Math.random()*2-1)/5e4,
         altitude:c().altitude
       }
     },_=>{})
